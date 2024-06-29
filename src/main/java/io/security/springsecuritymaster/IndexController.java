accf65e7028968552c977578bbea8dae8d48414e
Oh <osm723@gmail.com>
2024-06-29 19:43:14 +0900

    스프링 시큐리티 완전 정복 [6.x 개정판] 섹션 2. 인증 프로세스 로그 아웃 – logout() -2

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,5 +1,9 @@
 package io.security.springsecuritymaster;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,40 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/anonymous")
+    public String anonymous() {
+        return "anonymous";
+    }
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication) {
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "authentication";
+        } else {
+            return "not anonymous";
+        }
+    }
+
+    @GetMapping("/anonymousContext")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
+        return context.getAuthentication().getName();
+    }
+
+    @GetMapping("/logoutSuccess")
+    public String logoutSuccess() {
+        return "logoutSuccess";
+    }
+
+
 }
--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,25 +1,165 @@
 package io.security.springsecuritymaster;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    /*
+     *   formLogin 필터
+     */
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+//                //.formLogin(Customizer.withDefaults());
+//                .formLogin(form -> form
+//                        //.loginPage("/loginPage")
+//                        .loginProcessingUrl("/loginProc")
+//                        .defaultSuccessUrl("/", false)
+//                        .failureUrl("/failed")
+//                        .usernameParameter("userId")
+//                        .passwordParameter("passwd")
+//                        .successHandler(new AuthenticationSuccessHandler() {
+//                            @Override
+//                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                                System.out.println("authentication : " + authentication);
+//                                response.sendRedirect("/home");
+//                            }
+//                        })
+//                        .failureHandler(new AuthenticationFailureHandler() {
+//                            @Override
+//                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//                                System.out.println("exception : " + exception.getMessage());
+//                                response.sendRedirect("/login");
+//                            }
+//                        })
+//                        .permitAll()
+//                );
+//
+//        return http.build();
+//    }
+
+    /*
+     *   httpBasic 필터
+     */
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+//                //.httpBasic(Customizer.withDefaults());
+//                .httpBasic(basic -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+//
+//        return http.build();
+//    }
+
+    /*
+     *   rememberMe 필터
+     */
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+//                .formLogin(Customizer .withDefaults())
+//                .rememberMe(rememberMe -> rememberMe
+//                        .alwaysRemember(false)
+//                        .tokenValiditySeconds(3600)
+//                        .userDetailsService(userDetailsService())
+//                        .rememberMeParameter("remember")
+//                        .rememberMeCookieName("remember")
+//                        .key("security"));
+//
+//        return http.build();
+//    }
+
+    /*
+     *   anonymous 필터
+     */
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/anonymous").hasRole("GUEST")
+//                        .requestMatchers("/anonymousContext", "/authentication").permitAll()
+//                        .anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults())
+//                .anonymous(anonymous -> anonymous
+//                        .principal("guest")
+//                        .authorities("ROLE_GUEST")
+//                );
+//
+//        return http.build();
+//    }
+
+    /*
+    *   logout 필터
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/logoutSuccess").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                //.csrf(csrf ->csrf.disable())
+                .logout(logout -> logout
+                        .logoutUrl("/logoutProc")
+//                        .logoutRequestMatcher(new AntPathRequestMatcher("/logoutProc", "POST"))
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                        .logoutSuccessUrl("/logoutSuccess")
+                        .logoutSuccessHandler(new LogoutSuccessHandler() {
+                            @Override
+                            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                System.out.println("logoutSuccessHandler=====");
+                                response.sendRedirect("/logoutSuccess");
+                            }
+                        })
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .addLogoutHandler(new LogoutHandler() {
+                            @Override
+                            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                                System.out.println("addLogoutHandler======");
+                                HttpSession session = request.getSession();
+                                session.invalidate();
+                                SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
+                                SecurityContextHolder.getContextHolderStrategy().clearContext();
+                            }
+                        })
+                        .permitAll()
+                );
 
         return http.build();
     }
