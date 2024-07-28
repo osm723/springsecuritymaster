c4be547f9d56a6e082b21d8d880c3dff729e7d23
Oh <osm723@gmail.com>
2024-07-28 22:25:25 +0900

    스프링 시큐리티 완전 정복 [6.x 개정판] 섹션 7. 악용 보호 CORS (Cross Origin Resource Sharing) - 2

--- cors2/src/main/java/com/example/cors2/SecurityConfig.java
+++ cors2/src/main/java/com/example/cors2/SecurityConfig.java
@@ -2,13 +2,17 @@ package com.example.cors2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -16,19 +20,27 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        http.authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+                //.requestMatchers(HttpMethod.PUT, "/api/users").permitAll()
+                )
+                //.csrf(AbstractHttpConfigurer::disable)
         ;
+
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
         return http.build();
     }
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:8080");
-        configuration.addAllowedMethod("*");
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT"));
+        //configuration.addAllowedMethod("*");
+        //configuration.addAllowedMethod("GET");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
+        configuration.setMaxAge(1L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
 
