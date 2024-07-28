8294144e67ed11082fa9399fb1e343792fba1a96
Oh <osm723@gmail.com>
2024-07-28 17:43:53 +0900

    스프링 시큐리티 완전 정복 [6.x 개정판] 섹션 7. 악용 보호 CORS (Cross Origin Resource Sharing) - 2

---
+++ cors1/src/main/java/com/example/cors1/IndexController.java
@@ -0,0 +1,11 @@
+package com.example.cors1;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public String index() {
+        return "index";
+    }
+}
---
+++ cors2/src/main/java/com/example/cors2/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.cors2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        ;
+        return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+
+    }
+
+}
